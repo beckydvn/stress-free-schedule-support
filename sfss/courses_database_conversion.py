a4371521143e78d88f56eb3561ac7fcf9e6bbe23
beckydvn <18rldv@queensu.ca>
2022-02-24 23:47:03 -0500

    database all clean :grin:

--- sfss/courses_database_conversion.py
+++ sfss/courses_database_conversion.py
@@ -7,16 +7,16 @@ class Course(db.Model):
     credits = db.Column(db.Float, nullable=False, unique=False)
     course_name = db.Column(db.String, nullable=False, unique=False)
     description = db.Column(db.String, nullable=False, unique=False)
-    prerequisites = db.Column(db.PickleType, nullable=True, unique=False)
-    corequisites = db.Column(db.PickleType, nullable=True, unique=False)
-    exclusions = db.Column(db.PickleType, nullable=True, unique=False)
-    one_way_exclusions = db.Column(db.PickleType, nullable=True, unique=False)
-    equivalency = db.Column(db.PickleType, nullable=True, unique=False)
-    recommendations = db.Column(db.PickleType, nullable=True, unique=False)
+    prerequisites = db.Column(db.String, nullable=True, unique=False)
+    corequisites = db.Column(db.String, nullable=True, unique=False)
+    exclusions = db.Column(db.String, nullable=True, unique=False)
+    one_way_exclusions = db.Column(db.String, nullable=True, unique=False)
+    equivalency = db.Column(db.String, nullable=True, unique=False)
+    recommendations = db.Column(db.String, nullable=True, unique=False)
     learning_hours = db.Column(db.String, nullable=False, unique=False)
 
-    def __init__(self, id: str, credits: int, course_name: str, description: str, prerequisites: List[str], corequisites: List[str],
-                exclusions: List[str], one_way_exclusions: List[str], equivalency: List[str], recommendations: List[str], learning_hours: str) -> None:
+    def __init__(self, id: str, credits: int, course_name: str, description: str, prerequisites: str, corequisites: str,
+                exclusions: str, one_way_exclusions: str, equivalency: str, recommendations: str, learning_hours: str) -> None:
         super().__init__(id=id, credits=credits, course_name=course_name, description=description, prerequisites=prerequisites,
          corequisites=corequisites, exclusions=exclusions, one_way_exclusions=one_way_exclusions, equivalency=equivalency, recommendations=recommendations, learning_hours=learning_hours)
 
@@ -40,7 +40,6 @@ class Course(db.Model):
         return output
 
 def parse_description(line: str):
-    print(line)
     # split whenever you come across one of the following keywords
     keywords = ["PREREQUISITE ", "COREQUISITE ", "ONE-WAY EXCLUSION* ", "EXCLUSION ", "EQUIVALENCY ", "RECOMMENDATION ", "LEARNING HOURS "]
     splits = {}
@@ -49,17 +48,17 @@ def parse_description(line: str):
         # found an instance (shouldn't ever have more than 2 unless there's an error in the data)
         if len(find) == 2:
             before, after = find[0], find[1]
-            start_index = len(before)
+            start_index = len(before) + len(outer_k)
             # now we need to find where this section (i.e. prereqs) ends.
             # this is to avoid sections nesting inside each other and to achieve a clean split
-            end_index = start_index + len(after)
+            end_index = len(after)
             for inner_k in keywords:
                 find = after.split(inner_k)
                 if len(find) == 2:
-                    potential_end_index = start_index + len(find[0])
+                    potential_end_index = len(find[0])
                     if potential_end_index < end_index:
                         end_index = potential_end_index
-            splits[outer_k.strip(" ")] = line[start_index + len(outer_k) : end_index]
+            splits[outer_k.strip(" ")] = line[start_index : start_index + end_index]
     return splits
 
 def parse_course(line: str):
@@ -71,22 +70,40 @@ def create_database():
     new_course = True
     description = ""
     prerequisites = None
+    corequisites = None
+    one_way_exclusions = None
     exclusions = None
+    equivalency = None
     recommendations = None
+    learning_hours = None
 
     f = open("sfss/courses.txt", "r")
     text = f.readlines()
     f.close()    
     for i in range(len(text)):
-        print(i)
         line = text[i]
         if new_course:
             id, credits, course_name = parse_course(line)
             description = ""
             new_course = False
         elif line.strip(" ") == "\n":
-            description = parse_description(description)
-            db.session.add(Course(id, credits, course_name, description, prerequisites, corequisites, exclusions, equivalency, recommendations, learning_hours))
+            description_dict = parse_description(description)
+            for key, value in description_dict.items():
+                if key == "PREREQUISITE":
+                    prerequisites = value
+                elif key == "COREQUISITE":
+                    corequisites = value
+                elif key == "ONE-WAY EXCLUSION*":
+                    one_way_exclusions = value
+                elif key == "EXCLUSION":
+                    exclusions = value
+                elif key == "EQUIVALENCY":
+                    equivalency = value
+                elif key == "RECOMMENDATION":
+                    recommendations = value
+                elif key == "LEARNING HOURS":
+                    learning_hours = value
+            db.session.add(Course(id, credits, course_name, description, prerequisites, corequisites, exclusions, one_way_exclusions, equivalency, recommendations, learning_hours))
             new_course = True
         else:
             description += line
