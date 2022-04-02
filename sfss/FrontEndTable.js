24cb0ce4611b5dfad9a0fc4db789ab0506b87d2b
SLitwiniuk <68765886+SLitwiniuk@users.noreply.github.com>
2022-04-01 20:23:59 -0400

    Table Cell Changes

--- sfss/FrontEndTable.js
+++ sfss/FrontEndTable.js
@@ -1,13 +1,66 @@
 
-var Schedule = {
-    0: ["", "", "", "", "course1", "course1", "", "", "", "", "", "", "course3", "course3"]
+var Schedule = {0: ['', '', '', '', '', 'Course 4', 'Course 4', 'Course 4', '', '', 'Course 3', 'Course 3', 'Course 3', '', '', '', '', '', '', 'Course 1', 'Course 1', 'Course 1', '', '', '', '', '', ''], 
+1: ['', '', '', '', '', '', 'Course 0', 'Course 0', 'Course 0', 'Course 0', 'Course 0', 'Course 0', 'Course 0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
+2: ['', '', '', '', '', '', '', '', '', '', '', '', 'Course 1', 'Course 1', 'Course 1', '', 'Course 2', 'Course 2', 'Course 2', '', '', '', 'Course 1', 'Course 1', 'Course 1', '', '', ''], 
+3: ['Course 2', 'Course 2', 'Course 2', 'Course 3', 'Course 3', 'Course 3', 'Course 4', 'Course 4', 'Course 4', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
+4: ['', '', '', '', '', 'Course 2', 'Course 2', 'Course 2', '', '', '', 'Course 3', 'Course 3', 'Course 3', '', '', 'Course 4', 'Course 4', 'Course 4', '', '', '', '', '', '', '', '', '']
 }
 var colourOptions = ["tomato", "DodgerBlue", "rgb(146, 222, 76)", "rgb(106, 81, 255)", "rgb(255, 81, 81)", "rgb(255, 152, 77)"]
-var courseColours = {}
+var courseColours = {} 
+
+var colourOptions = ["tomato", "DodgerBlue", "rgb(146, 222, 76)", "rgb(106, 81, 255)", "rgb(255, 81, 81)", "rgb(255, 152, 77)"]
+
+var courseColours = {} 
+
+var rowspan = 1
+var courseTracking = "None"
+
+var cellToStartMerge = ["None", "None"] //The row and column of the starting cell [j, Day]
+var courseIndex = "None" //[index of last cell with course, the course name]
+var currCourse = "None"
+
 for (Day in Schedule){
     //I value corresponds to column number 
     for (var j = 0; j < Schedule[Day].length; j++) {
         cell = Schedule[Day][j]
+ 
+        if (cell != ""){
+            if ((cell == currCourse) && (courseIndex == j-1)){
+                rowspan += 1
+                courseIndex = j
+            }
+            else{
+                cellToStartMerge = [j, Day]
+                courseIndex = j
+                currCourse = cell
+                document.getElementById("r" + j + "c" + Day).innerHTML = cell //Add our current cell
+                if (cell in courseColours){
+                    document.getElementById("r" + j + "c" + Day).style.backgroundColor = courseColours[cell]
+                }
+                else{
+                    //var randomColour = Math.floor(Math.random()*16777215).toString(16);
+                    var randomColour = colourOptions[Math.floor(Math.random() * colourOptions.length)];
+                    document.getElementById("r" + j + "c" + Day).style.backgroundColor = randomColour
+                    courseColours[cell] = randomColour
+                }
+                if (rowspan > 1){
+                    var stringSpan = rowspan.toString(10)
+                    document.getElementById("r" +cellToStartMerge[0] + "c" + cellToStartMerge[1]).setAttribute("rowspan", stringSpan)
+                    rowspan = 1
+                }
+            }
+
+        }
+    } 
+}
+
+/*
+for (Day in Schedule){
+    //I value corresponds to column number 
+    for (var j = 0; j < Schedule[Day].length; j++) {
+        cell = Schedule[Day][j]
+        rowspan = 1
+        courseTracking = "None"
         if (cell != ""){
             document.getElementById("r" + j + "c" + Day).innerHTML = cell
             if (cell in courseColours){
@@ -22,6 +75,8 @@ for (Day in Schedule){
         }
     } 
 }
+*/
+
 //Translate: MON = rxc0
 //Tues = rxc1
 //WeD = rxc2
