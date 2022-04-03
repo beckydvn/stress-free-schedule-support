e59daedf9a4bf1063435f0e13d9eb86cc441b0e0
Ensaurus <18ehm1@queensu.ca>
2022-04-02 20:01:19 -0400

    small changes

--- sfss/static/js/plan_table.js
+++ sfss/static/js/plan_table.js
@@ -1,4 +1,4 @@
-var select = document.getElementById("selectStartTime");
+var select = document.getElementById("start0");
 var times = ["8:00AM", "8:30AM", "9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM", "5:00PM", "5:30PM", "6:00PM", "6:30PM", "7:00PM", "7:30PM", "8:00PM", "8:30PM", "9:00PM", "9:30PM", "10:00PM"]
 
 
@@ -16,6 +16,7 @@ function startChosen(parentid, value){
     lesson = document.getElementById(parentid);
     endTime = document.createElement("select");
     let oldChild = lesson.querySelector("#endTime");
+    endTime.classList.add("select");
     endTime.setAttribute("id", "endTime");
     console.log(oldChild);
     if (oldChild){
@@ -36,5 +37,5 @@ function startChosen(parentid, value){
 }
 
 function newLesson(parentid){
-    
+
 }
